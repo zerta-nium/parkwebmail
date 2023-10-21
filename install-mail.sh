7cb89b6f6980dfd87ab384e08b9e6e3c5d145ad1
zerta-nium <81821232+zerta-nium@users.noreply.github.com>
2023-10-22 00:30:13 +0200

    bugs fixed

--- install-mail.sh
+++ install-mail.sh
@@ -26,21 +26,18 @@ mkdir /var/www/html/postfixadmin/templates_c
 chown www-data: /var/www/html/postfixadmin/templates_c
 chown -R www-data: /var/www/html/postfixadmin/
 
-#SSL
-sudo certbot certonly --webroot -d $domain --email hadrien@zertanium.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
-
 # Backup original files
-mkdir ~/mailbackup
-sudo cp /etc/dovecot ~/mailbackup/dovecot
-sudo cp /etc/postfix ~/mailbackup/postfix
-sudo cp /etc/nginx ~/mailbackup/nginx
+sudo mkdir ~/mailbackup
+sudo cp -r /etc/dovecot ~/mailbackup/dovecot
+sudo cp -r /etc/postfix ~/mailbackup/postfix
+sudo cp -r /etc/nginx ~/mailbackup/nginx
 
 # Move new files
-find "~/parkwebmail/dovecot" -type f -exec mv {} "/etc/dovecot" \;
-find "~/parkwebmail/postfix" -type f -exec mv {} "/etc/postfix" \;
-find "~/parkwebmail/nginx" -type f -exec mv {} "/etc/nginx" \;
-sudo mv ~/parkwebmail/quota-warning.sh /usr/local/bin/quota-warning.sh
-sudo mv ~/parkwebmail/postfixadmin /var/www/html/postfixadmin/config.inc.php
+sudo find "$(pwd)/dovecot" -type f -exec mv {} "/etc/dovecot" \;
+sudo find "$(pwd)/parkwebmail/postfix" -type f -exec mv {} "/etc/postfix" \;
+sudo find "$(pwd)/parkwebmail/nginx" -type f -exec mv {} "/etc/nginx" \;
+sudo mv $(pwd)/parkwebmail/quota-warning.sh /usr/local/bin/quota-warning.sh
+sudo mv $(pwd)/parkwebmail/postfixadmin /var/www/html/postfixadmin/config.inc.php
 
 
 # MariaDB
@@ -53,32 +50,35 @@ FLUSH PRIVILEGES;
 EOF
 
 #Changes in file
-sed -i "s/maildomain/$domain/g" /etc/dovecot/conf.d/10-ssl.conf
-sed -i "s/maildomain/$domain/g" /etc/postfix/main.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/dovecot/dovecot-sql.conf.ext
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_mailbox_maps.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_maps.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_maps.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_domains_maps.cf
-sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sqlmysql_virtual_mailbox_maps.cf
-sed -i "s/changememail/$mailadmin_pass/g" /etc/postfix/sqlmysql_virtual_mailbox_maps.cf
-sed -i "s/changeme/$domain/g" /etc/nginx/sites-available/postfixadmin.conf
+sudo sed -i "s/maildomain/$domain/g" /etc/dovecot/conf.d/10-ssl.conf
+sudo sed -i "s/maildomain/$domain/g" /etc/postfix/main.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/dovecot/dovecot-sql.conf.ext
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_catchall_maps.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_mailbox_maps.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_domain_maps.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_alias_maps.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sql/mysql_virtual_domains_maps.cf
+sudo sed -i "s/changeme/$mailserver_pass/g" /etc/postfix/sqlmysql_virtual_mailbox_maps.cf
+sudo sed -i "s/changememail/$mailadmin_pass/g" /etc/postfix/sqlmysql_virtual_mailbox_maps.cf
+sudo sed -i "s/changeme/$domain/g" /etc/nginx/sites-available/postfixadmin.conf
 
 ## Postfix
-chmod 0640 /etc/postfix/sql/*
-chgrp postfix /etc/postfix/sql/mysql-*.cf
-chmod u=rw,g=r,o= /etc/postfix/sql/mysql-*.cf
+sudo chmod 0640 /etc/postfix/sql/*
+sudo chgrp postfix /etc/postfix/sql/mysql-*.cf
+sudo chmod u=rw,g=r,o= /etc/postfix/sql/mysql-*.cf
 
 ## DOVECOT
-groupadd -g 5000 vmail
-useradd -g vmail -u 5000 vmail -d /var/vmail -m
-chown -R vmail:vmail /var/vmail
-chown root:root /etc/dovecot/dovecot-sql.conf.ext
-chmod go= /etc/dovecot/dovecot-sql.conf.ext
-chmod +x /usr/local/bin/quota-warning.sh
+sudo groupadd -g 5000 vmail
+sudo useradd -g vmail -u 5000 vmail -d /var/vmail -m
+sudochown -R vmail:vmail /var/vmail
+sudo chown root:root /etc/dovecot/dovecot-sql.conf.ext
+sudo chmod go= /etc/dovecot/dovecot-sql.conf.ext
+sudo chmod +x /usr/local/bin/quota-warning.sh
 sudo gpasswd -a www-data dovecot
 sudo setfacl -R -m u:www-data:rwx /var/run/dovecot/stats-reader /var/run/dovecot/stats-writer
+
+#SSL
+sudo certbot certonly --webroot -d $domain --email hadrien@zertanium.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
 
 ## End of script
 echo "The mail server is installed."
@@ -88,7 +88,7 @@ echo "Mailserver Pass: $mailserver_pass"
 echo "Domain Name:  $domain"
 echo "------------------------------------------------"
 
-cat << EOF >> ~/mailbackup/mail.txt
+sudo cat << EOF >> ~/mailbackup/mail.txt
 ------------------------------------------------
 Mailadmin Pass: $(mailadmin_pass)
 Mailserver Pass: $(mailserver_pass)
